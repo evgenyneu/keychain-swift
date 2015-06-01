bf5dad621a673f968be57e628877a9cc2e4731bb
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-06-01 13:39:22 +1000

    Add framework target

---
+++ KeychainSwift/KeychainSwift.h
@@ -0,0 +1,19 @@
+//
+//  KeychainSwift.h
+//  KeychainSwift
+//
+//  Created by Evgenii on 1/06/2015.
+//  Copyright (c) 2015 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeychainSwift.
+FOUNDATION_EXPORT double KeychainSwiftVersionNumber;
+
+//! Project version string for KeychainSwift.
+FOUNDATION_EXPORT const unsigned char KeychainSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeychainSwift/PublicHeader.h>
+
+
