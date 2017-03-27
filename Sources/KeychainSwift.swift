9bb711ee5c28f8cc7e1a04d02468fa2d2f7ee3b3
Craig Siemens <siemens.craig@gmail.com>
2017-03-27 14:03:39 -0600

    Fix warning from Xcode 8.3. Fixes #55

--- Sources/KeychainSwift.swift
+++ Sources/KeychainSwift.swift
@@ -136,7 +136,7 @@ open class KeychainSwift {
   open func get(_ key: String) -> String? {
     if let data = getData(key) {
       
-      if let currentString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as? String {
+      if let currentString = String(data: data, encoding: .utf8) {
         return currentString
       }
       
