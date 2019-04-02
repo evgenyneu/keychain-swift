ef33fb1e1fb6075795d29652c23f7710280eb3a9
Stephen Hayes <shayes20@its.jnj.com>
2019-04-02 13:02:30 -0400

    Fixes #97, upgraded to Swift 5

--- Sources/KeychainSwift.swift
+++ Sources/KeychainSwift.swift
@@ -122,7 +122,7 @@ open class KeychainSwift {
     withAccess access: KeychainSwiftAccessOptions? = nil) -> Bool {
   
     let bytes: [UInt8] = value ? [1] : [0]
-    let data = Data(bytes: bytes)
+    let data = Data(bytes)
 
     return set(data, forKey: key, withAccess: access)
   }
@@ -167,7 +167,7 @@ open class KeychainSwift {
     var query: [String: Any] = [
       KeychainSwiftConstants.klass       : kSecClassGenericPassword,
       KeychainSwiftConstants.attrAccount : prefixedKey,
-      KeychainSwiftConstants.returnData  : kCFBooleanTrue,
+      KeychainSwiftConstants.returnData  : kCFBooleanTrue!,
       KeychainSwiftConstants.matchLimit  : kSecMatchLimitOne
     ]
     
